5d64c9bfe86035e26132de7bab835e5f314f96e2
egorrya <fayncore@gmail.com>
2022-11-21 15:20:52 +0300

    build

--- config/paths.js
+++ config/paths.js
@@ -3,5 +3,5 @@ const path = require('path');
 module.exports = {
   src: path.resolve(__dirname, '../src'),
   assets: path.resolve(__dirname, '../assets'),
-  build: path.resolve(__dirname, '../dist'),
+  build: path.resolve(__dirname, '../docs'),
 };
